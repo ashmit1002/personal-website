086cdf0c371fe9d3cefa3497ec161a63d1778826
ashmit1002 <ashmits1002@gmail.com>
2024-09-28 21:06:42 -0400

    navbar update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-// Navbar.js
+// src/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -6,24 +6,16 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-800 text-white shadow-lg">
       <ul className="flex justify-center space-x-8 py-4">
-        <li>
-          <Link to="/" className="text-lg hover:text-gray-300 transition duration-300">Home</Link>
-        </li>
-        <li>
-          <Link to="/about" className="text-lg hover:text-gray-300 transition duration-300">About</Link>
-        </li>
-        <li>
-          <Link to="/education" className="text-lg hover:text-gray-300 transition duration-300">Education</Link>
-        </li>
-        <li>
-          <Link to="/skills" className="text-lg hover:text-gray-300 transition duration-300">Skills</Link>
-        </li>
-        <li>
-          <Link to="/experience" className="text-lg hover:text-gray-300 transition duration-300">Experience</Link>
-        </li>
-        <li>
-          <Link to="/contact" className="text-lg hover:text-gray-300 transition duration-300">Contact</Link>
-        </li>
+        {['Home', 'About', 'Projects', 'Education', 'Skills', 'Contact'].map((item) => (
+          <li key={item}>
+            <Link
+              to={`/${item.toLowerCase()}`} // Links will be in lowercase
+              className="text-lg transition duration-300 transform hover:scale-105 hover:text-gray-300 active:scale-95"
+            >
+              {item}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
