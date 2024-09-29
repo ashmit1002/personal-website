608cfc8b2b1df9cd8b8ab71f9dd6763ee463d1aa
ashmit1002 <ashmits1002@gmail.com>
2024-09-28 20:49:05 -0400

    auto load 2.0

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Header from './Header';
 import About from './About';
-import Projects from './Projects'; // This includes your combined Projects and Experience sections
+import Experience from './Experience'; // This includes your combined Projects and Experience sections
 import Education from './Education';
 import Skills from './Skills';
 import Contact from './Contact';
@@ -18,7 +18,7 @@ function App() {
           {/* Default route to render the Homepage */}
           <Route path="/" element={<Header />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
           <Route path="/education" element={<Education />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/contact" element={<Contact />} />
